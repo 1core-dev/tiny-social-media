66341b96efdde4d4a93738854196a5bf63251f9b
1core-dev <1core@mail.com>
2022-02-07 10:18:18 +0200

    Modified posts endpoints, posts service

--- src/app/api/posts.py
+++ src/app/api/posts.py
@@ -2,46 +2,59 @@ from typing import List
 
 from fastapi import APIRouter, Depends, Response, status
 
+from ..models.auth import User
 from ..models.posts import Post, PostCreate, PostUpdate
+from ..services.auth import get_current_user
 from ..services.posts import PostsService
 
 router = APIRouter(prefix='/posts')
 
 
 @router.get('/', response_model=List[Post])
-def get_posts(service: PostsService = Depends()):
-    return service.get_posts()
+def get_posts(
+        user: User = Depends(get_current_user),
+        service: PostsService = Depends()
+):
+    return service.get_posts(user_id=user.id)
 
 
 @router.post('/', response_model=Post)
 def create_post(
         post_data: PostCreate,
+        user: User = Depends(get_current_user),
         service: PostsService = Depends()
 ):
-    return service.create_post(post_data)
+    return service.create_post(user_id=user.id, post_data=post_data)
 
 
 @router.get('/{post_id}', response_model=Post)
 def get_post(
         post_id: int,
+        user: User = Depends(get_current_user),
         service: PostsService = Depends()
 ):
-    return service.get_post(post_id)
+    return service.get_post(user_id=user.id, post_id=post_id)
 
 
 @router.put('/{post_id}', response_model=Post)
 def update_post(
         post_id: int,
         post_data: PostUpdate,
+        user: User = Depends(get_current_user),
         service: PostsService = Depends()
 ):
-    return service.update_post(post_id, post_data)
+    return service.update_post(
+        user_id=user.id,
+        post_id=post_id,
+        post_data=post_data
+    )
 
 
 @router.delete('/{post_id}')
 def delete_post(
         post_id: int,
+        user: User = Depends(get_current_user),
         service: PostsService = Depends()
 ):
-    service.delete_post(post_id)
+    service.delete_post(user_id=user.id, post_id=post_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- src/app/services/posts.py
+++ src/app/services/posts.py
@@ -12,42 +12,48 @@ class PostsService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
-    def _get(self, post_id: int) -> tables.Post:
+    def _get(self, user_id: int, post_id: int) -> tables.Post:
         post = (
             self.session
                 .query(tables.Post)
-                .filter_by(id=post_id)
+                .filter_by(
+                id=post_id,
+                user_id=user_id
+            )
                 .first()
         )
         if not post:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
         return post
 
-    def get_posts(self) -> List[tables.Post]:
+    def get_posts(self, user_id: int) -> List[tables.Post]:
         posts = (
             self.session
                 .query(tables.Post)
+                .filter_by(user_id=user_id)
                 .all()
         )
         return posts
 
-    def get_post(self, post_id: int) -> tables.Post:
-        return self._get(post_id)
+    def get_post(self, user_id: int, post_id: int) -> tables.Post:
+        return self._get(user_id, post_id)
 
-    def create_post(self, post_data: PostCreate) -> tables.Post:
-        post = tables.Post(**post_data.dict())
+    def create_post(self, user_id: int, post_data: PostCreate) -> tables.Post:
+        post = tables.Post(
+            **post_data.dict(),
+            user_id=user_id)
         self.session.add(post)
         self.session.commit()
         return post
 
-    def update_post(self, post_id: int, post_data: PostUpdate) -> tables.Post:
-        post = self._get(post_id)
+    def update_post(self, user_id: int, post_id: int, post_data: PostUpdate) -> tables.Post:
+        post = self._get(user_id, post_id)
         for field, value in post_data:
             setattr(post, field, value)
         self.session.commit()
         return post
 
-    def delete_post(self, post_id: int) -> None:
-        post = self._get(post_id)
+    def delete_post(self, user_id: int, post_id: int) -> None:
+        post = self._get(user_id, post_id)
         self.session.delete(post)
         self.session.commit()
